60ac2e341f0c09bab4d27d866ac4a16b71f3d6f8
Andres Kalev <andres.kalev@fleetcomplete.eu>
2020-11-27 09:58:34 +0200

    Fixed source not found

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "indent": ["error", 4],
+    "semi": ["error", "always"],
+    "comman-dangle": "off"
   }
 }
---
+++ docs/precache-manifest.2032af789ce9f009cc988eb3c64dac54.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b38469716a669e58c4",
+    "url": "/vuepwa/css/app.3ff004a8.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/vuepwa/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "1eaab24f9296a12624340b24a7c8ae75",
+    "url": "/vuepwa/index.html"
+  },
+  {
+    "revision": "3436ea3ac82482f3e3a0",
+    "url": "/vuepwa/js/about.063687bd.js"
+  },
+  {
+    "revision": "62b38469716a669e58c4",
+    "url": "/vuepwa/js/app.dc075439.js"
+  },
+  {
+    "revision": "898de33fbddfef3c36dd",
+    "url": "/vuepwa/js/chunk-vendors.b562ad2b.js"
+  },
+  {
+    "revision": "0cecec95db209a32781eb20aa8416046",
+    "url": "/vuepwa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vuepwa/robots.txt"
+  }
+]);
